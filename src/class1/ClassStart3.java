495ecd5280bdac1bce4497c835037b56192c5b2e
vanhelsing <vanhelsing@naver.com>
2024-02-24 00:30:44 +0900

    클래스와 사용자타입, 객체에 대한 설명을 위한 예제소스

--- src/class1/ClassStart3.java
+++ src/class1/ClassStart3.java
@@ -2,9 +2,10 @@ package class1;
 
 public class ClassStart3 {
     public static void main(String[] args) {
-        Student student1;
-        student1 = new Student();
-        student1.name = "학생1";
+        Student student1;           //1. Student 객체 생성 = Student변수 선언 = Student 타입을 받을 수 있는 변수를 선언
+                                    //   int는 정수, String은 문자를 담을 수 있듯이 Student는 Student타입의 객체를 받을 수 있므
+        student1 = new Student();   //2. new Student()의 결과로 x001 참조값 반환
+        student1.name = "학생1";     //3. 최종 결과 student1 = x001;
         student1.age = 15;
         student1.grade = 90;
 
